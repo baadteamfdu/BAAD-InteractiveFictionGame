cae1097a9d7ae4a7610eba074f5d534cc73ec3a8
abehrensFDU <a.behrens@student.fdu.edu>
2025-10-09 23:28:49 -0400

    BAAD-21 Fixed private methods

--- BAAD-InteractiveFictionGame/Inventory.h
+++ BAAD-InteractiveFictionGame/Inventory.h
@@ -7,11 +7,13 @@ using namespace std;
 
 class Inventory
 {
-private:
-	vector<Object>objects; // a vector list got created all the objects will get stored here.
-	
+
+public:
 	void addObject(Object object);  // adds an item to the inventory.
 	void showInventory();           // suppose to display all the items in the inventory.
 	bool gotObject(string objectName); // checks if the person has the items. 
+private:
+	vector<Object>objects; // a vector list got created all the objects will get stored here.
+	
 };
 
