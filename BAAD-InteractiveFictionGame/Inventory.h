09dbe03a86025791d5bf0578169c5668dbc45267
daved1173 <daved1173@gmail.com>
2025-10-11 17:11:15 -0400

    Update Inventory.h
    Created the inventory header file.

--- BAAD-InteractiveFictionGame/Inventory.h
+++ BAAD-InteractiveFictionGame/Inventory.h
@@ -1,10 +1,21 @@
 #pragma once
-#include "Object.h"
 #include <string>
-using namespace std;
 
+class Object;
 
-class Inventory
+class Inventory 
 {
+public:
+    Inventory();
+    ~Inventory();
+
+    bool addItem(const Object& obj);
+    bool hasItem(const string& name) const;
+    const Object* getItem(const string& name) const;
+    string showInventory() const;
+
+private:
+    class Impl;
+    Impl* impl_;
 };
 
