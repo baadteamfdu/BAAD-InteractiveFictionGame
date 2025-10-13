5327bee1d7ae906d0c6e7f1b3592782718f0a36d
daved1173 <daved1173@gmail.com>
2025-10-13 09:33:08 -0400

    BAAD-21 removed the constant member

--- BAAD-InteractiveFictionGame/Inventory.h
+++ BAAD-InteractiveFictionGame/Inventory.h
@@ -13,8 +13,8 @@ public:
 	void showInventory();           // suppose to display all the items in the inventory.
 	bool gotObject(string objectName); // checks if the person has the items. 
 
-	Object* getObject(const string& objectName);
-    const Object* getObject(const string& objectName) const;
+	Object* getObject(string objectName);
+
 private:
 	vector<Object>objects; // a vector list got created all the objects will get stored here.
 	
