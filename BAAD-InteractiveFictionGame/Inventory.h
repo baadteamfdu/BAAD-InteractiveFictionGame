a83881b2d14d048956b44102f090fe1fa5b40f9a
daved1173 <daved1173@gmail.com>
2025-10-13 09:24:59 -0400

    BAAD-21 added get object
    the first one is so that you can modify the object, the second one is for reading the object!

--- BAAD-InteractiveFictionGame/Inventory.h
+++ BAAD-InteractiveFictionGame/Inventory.h
@@ -12,6 +12,9 @@ public:
 	void addObject(Object object);  // adds an item to the inventory.
 	void showInventory();           // suppose to display all the items in the inventory.
 	bool gotObject(string objectName); // checks if the person has the items. 
+
+	Object* getObject(const string& objectName);
+    const Object* getObject(const string& objectName) const;
 private:
 	vector<Object>objects; // a vector list got created all the objects will get stored here.
 	
