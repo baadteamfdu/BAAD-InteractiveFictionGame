0c0faa8dcf9b72537440c427bec8b203a223356c
daved1173 <daved1173@gmail.com>
2025-10-12 23:35:00 -0400

    BAAD-21 added the functions getObject, addObject, and removeObject. Also created a vectir.

--- BAAD-InteractiveFictionGame/Room.h
+++ BAAD-InteractiveFictionGame/Room.h
@@ -20,3 +20,8 @@ public:
 	string getDescription();
 };
 
+Object* getObject(string name); //gets the object but returns nullptr if the object couldn't be found
+void addObject(Object* object); //adds an object to the roomObjects vector 
+void removeObject(string name); //removes the object from the roomObjects vector
+vector<Object*> roomObjects;     
+
