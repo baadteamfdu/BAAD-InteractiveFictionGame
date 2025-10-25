836414c41c1a9686be554f31a4d4be84850b48db
abehrensFDU <a.behrens@student.fdu.edu>
2025-10-24 21:59:05 -0400

    Moved some methods to private

--- BAAD-InteractiveFictionGame/Alien.h
+++ BAAD-InteractiveFictionGame/Alien.h
@@ -9,13 +9,9 @@ class Alien {
         Alien();
         void setActive(bool active); //returns a bool called isActive
         bool getIsActive(); //returns state of alien
-        void setSawPlayer(bool seen); //sets state of alien
-        bool getSawPlayer(); //returns bool sawPlayer
         void increaseTurnCounter(Room* playerCurrentRoom); //increases value of turns counters
-        void move(); //on first(alienCurrentRoom = nullptr) activate it goes to cryoHall set as AliencurrentRoom else leave()
-        void killPlayer(); //tell player they lost and exit game
-        void leave(); //leave setSawPlayer(false) and determine new slightly random threshold and what the new room will eventually be(maybe dont make it able to enter the same exact room twice in a row)
         void addRoom(Room* newRoom);
+        void move(); //on first(alienCurrentRoom = nullptr) activate it goes to cryoHall set as AliencurrentRoom else leave()
 private:
         bool isActive = false; //store if alien is active
         bool sawPlayer = false;//store if alien has seen the player
@@ -25,4 +21,9 @@ private:
         int killThreshold = 1; //store a threshold for kills will be set randomly later
         Room* alienCurrentRoom = nullptr; //store aliens current Room
         vector<Room*> roomList;
+        void killPlayer(); //tell player they lost and exit game
+        void setSawPlayer(bool seen); //sets state of alien
+        bool getSawPlayer(); //returns bool sawPlayer
+        void leave(); //leave setSawPlayer(false) and determine new slightly random threshold and what the new room will eventually be(maybe dont make it able to enter the same exact room twice in a row)
+
 };
