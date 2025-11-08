fa844e58b62c4cb00ed116689cfe16250e1718a3
Lizst-cv <windy33e@gmail.com>
2025-11-08 14:43:23 -0500

    BAAD-104 Added deleteObject function in the Inventory.cpp

--- BAAD-InteractiveFictionGame/Game.h
+++ BAAD-InteractiveFictionGame/Game.h
@@ -23,7 +23,7 @@ private:
     vector<Room*> allRooms;             //this vector stores all rooms for the map
 
     bool playerIsHidden = false;     // flag to check if the player is hidden. Consider creating new player class later.
-    
+
 public:
 
     void init();
@@ -32,19 +32,20 @@ public:
 
     Room* getCurrentRoom();
     void setCurrentRoom(Room* nextRoom);
-  
-    void peekDoor(const string & doorName); //peek that just checks if next room has alien in it or not and notifies player
-	void useKeycard(Object* door);
+
+    void peekDoor(const string& doorName); //peek that just checks if next room has alien in it or not and notifies player
+    void useKeycard(Object* door);
     void useScrewdriver(Object* vent);
-	void goDoor(const string& doorName);
-    void typeCode(int enteredCode); 
+    void goDoor(const string& doorName);
+    void typeCode(int enteredCode);
+    bool combine(Object* one, Object* two); // to combine two objects
 
     void displayMap() const; //this code will display the map for the game
     void hide(string noun);
-	void unhide();
+    void unhide();
 
-	void setIsHidden(bool hidden); // setter for playerIsHidden
+    void setIsHidden(bool hidden); // setter for playerIsHidden
 
-	bool getIsHidden(); // getter for playerIsHidden
+    bool getIsHidden(); // getter for playerIsHidden
 
-};
+}
